e82ba7ec7f6b2b8948a90cf291babd37990b04ce
Karl Withers <65951397+karlwith@users.noreply.github.com>
2021-11-07 15:51:51 +1000

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -1,20 +1,3 @@
-'''
-Framework designed to assist with evaluation of ARIMA model configurations for time series imputation of Bluetooth MAC
-address scanner (BMS) traffic data.
-
-Insights into initial ARIMA model configuration are given by Autocorrelation and Partial Autocorrelation plots. All
-valid ARIMA model configurations are deployed to impute randomly seeded BMS time series gaps of sizes 1, 2, 3 and 4
-time lag intervals of 5 minutes.
-
-Root mean square error is used to evaluate the performance of each model configuration
-for all gap sizes. A basic linear interpolation model is also implemented as a benchmark and found to be the most
-effective at imputing single point gaps.
-
-Author: Karl Withers
-Work completed individually as part of QUT research unit IFN712
-'''
-
-
 import math
 import operator
 import pandas as pd
@@ -183,6 +166,7 @@ class Model:
     err_quad_gap: float
     err_avg: float
 
+        
 if __name__ == '__main__':
     ##
     ## LOAD DATASETS
